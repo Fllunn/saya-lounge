ed6e959882389bf290c39158498b2804d56c51a3
Jet-Green <grishadzyin@gmail.com>
2025-11-09 15:11:04 +0500

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar flat border>
+      <v-container class="d-flex align-center">
+        <NuxtLink to="/" class="text-decoration-none text-high-emphasis">
+          <div class="font-weight-bold text-h6">Сая</div>
+        </NuxtLink>
+
+        <v-spacer></v-spacer>
+
+        <div class="d-flex align-center">
+        </div>
+      </v-container>
+    </v-app-bar>
+
+    <v-main>
+      <slot />
+    </v-main>
+
+    <Footer />
+  </v-app>
+</template>
